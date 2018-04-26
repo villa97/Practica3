19a6cc48bf92e61fcf807342f180919025c99407
Villa.Cr <30905769+villa97@users.noreply.github.com>
2018-04-25 19:58:36 -0500

    MAIN
    Se agrega el template con el cual se trabajara el codigo.

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <18F4620.h>
+#fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
+#fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
+#use delay(clock=16000000)
+
+#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
+
+#ifdef __DEBUG_SERIAL__
+   #define TX_232        PIN_C6
+   #use RS232(BAUD=9600, XMIT=TX_232, BITS=8,PARITY=N, STOP=1)
+   #use fast_io(c)
+#endif
+
+void main (void){
+   setup_oscillator(OSC_16MHZ);
+#ifdef __DEBUG_SERIAL__ //Deberiamos de proteger nuestras depuraciones de esta forma o usar una macro ya protegida.
+   printf("Hola Mundo\n");//Puedes usar putc o printf. Revisa la documentación de CCS para ver que mas puedes hacer.
+#endif
+   while(1){
+      
+   }
+}
